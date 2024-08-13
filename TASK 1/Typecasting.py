db12c5702462102bd0b7c69dc82883718c0552fb
SOHEB-DS <94733617+SOHEB-DS@users.noreply.github.com>
2024-08-13 15:38:59 +0530

    Add files via upload

---
+++ TASK 1/Typecasting.py
@@ -0,0 +1,35 @@
+# Define the variables
+a = 10          # Integer
+b = 10.5        # Float
+c = 3 + 4j     # Complex
+
+a_float = float(a)
+
+# Convert integer to complex
+a_complex = complex(a)
+
+# Convert float to integer
+b_integer = int(b)
+
+# Convert float to complex
+b_complex = complex(b)
+
+
+c_float = str(e)
+c_integer = str(e)
+
+# Convert integer, float, and complex to string
+a_string = str(a)
+b_string = str(b)
+c_string = str(c)
+
+# Print the results
+print("Integer to Float:", a_float)
+print("Integer to Complex:", a_complex)
+print("Float to Integer:", b_integer)
+print("Float to Complex:", b_complex)
+print("Complex to Float:", c_float)
+print("Complex to Integer:", c_integer)
+print("Integer to String:", a_string)
+print("Float to String:", b_string)
+print("Complex to String:", c_string)
